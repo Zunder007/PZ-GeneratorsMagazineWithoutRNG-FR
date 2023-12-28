6f13d6cc3f9cfd9eb2732ccd31d39a6f46bc2b85
Zunder <108587289+Zunder007@users.noreply.github.com>
2023-12-28 08:07:05 +0100

    Update Generator.lua

--- Generator.lua
+++ Generator.lua
@@ -4,7 +4,7 @@ function LevelPerkTest(character, perk, level, levelUp)
 	if levelUp and level == 3 and tostring(perk) == "Electricity" then
 		print("[Generator] LevelPerkTest")
 		character:getInventory():AddItem("Base.ElectronicsMag4")
-		character:Say("Oh! Maintenant je comprends ce magazine. Il est question de faire du jus... mais pas du jus de tomates!")
+		character:Say("Oh! Maintenant je comprends ce magazine. Il explique le fonctionnement de cet appareil pour produire du courant.")
 	end
 end
 
